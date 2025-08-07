91d28aecdaf11e885ca1bb410b41052c89e06b13
Tamim <tamimvaiya.123@gmail.com>
2025-08-07 23:27:46 +0600

    Handled POST Request

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,10 @@ from .forms import ExpenseForm
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+            
     expense_form = ExpenseForm()
-
     return render(request, 'myapp/index.html', {'expense_form' : expense_form})
