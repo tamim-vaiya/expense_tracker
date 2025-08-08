c52474f49179ac7651cc0f98ed0c40fd3bd03295
Tamim <tamimvaiya.123@gmail.com>
2025-08-08 22:06:13 +0600

    Added Expense List

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import ExpenseForm
+from .models import Expense
 
 # Create your views here.
 def index(request):
@@ -7,6 +8,7 @@ def index(request):
         expense = ExpenseForm(request.POST)
         if expense.is_valid():
             expense.save()
-            
+
+    expenses = Expense.objects.all()
     expense_form = ExpenseForm()
-    return render(request, 'myapp/index.html', {'expense_form' : expense_form})
+    return render(request, 'myapp/index.html', {'expense_form' : expense_form, 'expenses': expenses})
