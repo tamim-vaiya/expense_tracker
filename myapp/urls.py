b61be6d873de2d6a8a43764a53de484929c1674c
Tamim <tamimvaiya.123@gmail.com>
2025-08-10 00:21:59 +0600

    Added Delete Functionality

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, edit
+from .views import index, edit, delete
 
 urlpatterns = [
     path('', index, name='index'),
     path('edit/<int:id>/', edit, name="edit"),
+    path('delete/<int:id>/', delete, name="delete"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -23,3 +23,9 @@ def edit(request, id):
             form.save()
             return redirect('index')
     return render(request, 'myapp/edit.html', {'expense_form' : expense_form})
+
+def delete(request, id):
+    if request.method == "POST" and 'delete' in request.POST:
+        expense = Expense.objects.get(id=id)
+        expense.delete()
+    return redirect('index')
