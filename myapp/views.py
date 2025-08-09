7c684a54566041713924337b831c3c1f3c487dd1
Tamim <tamimvaiya.123@gmail.com>
2025-08-09 18:52:44 +0600

    Added Edit Functionality and Designed the page
    '
    .q
    s

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ExpenseForm
 from .models import Expense
 
@@ -12,3 +12,14 @@ def index(request):
     expenses = Expense.objects.all()
     expense_form = ExpenseForm()
     return render(request, 'myapp/index.html', {'expense_form' : expense_form, 'expenses': expenses})
+
+def edit(request, id):
+    expense = Expense.objects.get(id=id)
+    expense_form = ExpenseForm(instance=expense)
+    if request.method == "POST":
+        expense = Expense.objects.get(id=id)
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'myapp/edit.html', {'expense_form' : expense_form})
