0cce7faa42c323f5c2b8cffb970ce5d1d4dbb483
Tamim <tamimvaiya.123@gmail.com>
2025-08-10 20:19:28 +0600

    Added Total row and humanize

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'myapp',
     'widget_tweaks',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
