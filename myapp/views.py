7cd1c159526c93994bc7f5ed13d290048718e65a
Tamim <tamimvaiya.123@gmail.com>
2025-08-12 00:07:09 +0600

    Added Total Across Week, MOnth, Year

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import ExpenseForm
 from .models import Expense
 from django.db.models import Sum
+import datetime
 
 # Create your views here.
 def index(request):
@@ -12,8 +13,24 @@ def index(request):
 
     expenses = Expense.objects.all()
     total_expenses = expenses.aggregate(Sum('amount'))
+
+    # logic to calculate 365 days expenses
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = data.aggregate(Sum('amount'))
+
+    # logic to calculate last 30 days expenses
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = data.aggregate(Sum('amount'))
+
+    # logic to calculate last 7 days expenses
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = data.aggregate(Sum('amount'))
+
     expense_form = ExpenseForm()
-    return render(request, 'myapp/index.html', {'expense_form' : expense_form, 'expenses': expenses, 'total_expenses': total_expenses})
+    return render(request, 'myapp/index.html', {'expense_form' : expense_form, 'expenses': expenses, 'total_expenses': total_expenses, 'yearly_sum': yearly_sum, 'monthly_sum': monthly_sum,  'weekly_sum': weekly_sum})
 
 def edit(request, id):
     expense = Expense.objects.get(id=id)
